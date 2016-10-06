10a32b84fc7d83e65b0a2154a360c7622859429d
pnakibar <pedronakibar@gmail.com>
2016-10-06 18:46:30 -0300

    finished!

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Running
+```bash
+  npm run dev
+```
+And open the *index.html*, located in the root directory of this project, in your browser!
+
 # Building
 ```bash
   npm run build
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import cors from 'cors'; // nova dependência! para fazer o login
 const users = [
   { name: 'user1', password: '123' },
   { name: 'user2', password: '123' }
-];
+]; // usuários padrão
 
 const SECRET = 'secret'; // insira algo mais seguro!
 
@@ -21,17 +21,14 @@ const server = new http.Server(app);
 const io = socketio.listen(server);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`[x] Magic happens on port: ${port}`));
+server.listen(port, () => console.log(`[x] Magic happens on port: ${port}`));
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  console.log('whatzass');
-  return res.json({ hello: 'world!' });
-});
+app.get('/', (req, res) => res.json({ hello: 'world!' }));
 
 // registro de novo usuário
 app.post('/signup', (req, res) => {
@@ -46,7 +43,7 @@ app.post('/signup', (req, res) => {
 app.post('/login', (req, res) => {
   try {
     const { name, password } = req.body;
-    const user = users.filter(u => u.name === name && u.password === password);
+    const user = users.filter(u => u.name === name && u.password === password)[0];
     if (user) {
       return res.status(200).json(jwt.sign({ name: user.name }, SECRET));
     }
@@ -58,16 +55,18 @@ app.post('/login', (req, res) => {
 
 // definir o middleware
 io.use(socketioJwt.authorize({
-  secret: 'secret',
+  secret: SECRET,
   handshake: true
 }));
 
 io.on('connection', (socket) => {
+  const userName = socket.decoded_token.name; // nome do usuário, que vem pelo token de login
   socket.emit('messages', messages);
   socket.emit('hello', 'Welcome!');
   socket.on('sent message', (message) => {
-    messages.push(message);
-    io.emit('new message', message);
+    const newMessage = { userName, message }; // alteramos a estrutura das mensagens, temos que atualizar no front-end!
+    messages.push(newMessage);
+    io.emit('new message', newMessage);
   });
 });
 
